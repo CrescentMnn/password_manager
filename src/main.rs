c4d09e9ec0bf01701e3b49fdf24cf7df6b6e55c7
Moon <moon@pulsar.ipv6.infinitum.net.mx>
2024-06-03 23:29:41 -0500

    Added a hash/verify test fn

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,26 @@ fn main() {
     println!("Password: {}", test_password);
     println!("Hashed password: {}", hash_password);
 }
+
+#[test]
+fn test_hashing(){
+
+    {
+        let test_password = "123123";
+        let hashed_test_pssw = hash(test_password, DEFAULT_COST).expect("(-) Failed to hash password");
+
+        let hash_valid = verify(test_password, &hashed_test_pssw).expect("(-) Failed to verify password");
+
+        assert_eq!(hash_valid, true);
+    }
+
+    {
+        let test_password = "u.9,6Pjz;YdV)Z@Cv^LeW2";
+        let hashed_test_pssw = hash(test_password, DEFAULT_COST).expect("(-) Failed to hash password");
+
+        let hash_valid = verify(test_password, &hashed_test_pssw).expect("(-) Failed to verify password");
+
+        assert_eq!(hash_valid, true);
+    }
+
+}
