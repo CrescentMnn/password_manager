d4fd8e8af797f850faaa7ba077ba14e74394ae85
Moon <moon@pulsar.ipv6.infinitum.net.mx>
2024-06-04 01:31:09 -0500

    Added a test io function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
 extern crate bcrypt;
 
 use bcrypt::{hash, verify, DEFAULT_COST};
+use std::io;
 
 fn main() {
 
     println!("+-----------------------------------+");
     println!("+           Password Manager        +");
-    println!("+-----------------------------------+");
+    println!("+-----------------------------------+\n\n");
+    
+
+
+    println!("This is a simple password manager program which lets users securely store, manage, and retrieve passwords. This project uses the bcrypt library to hash and validate passwords, ensuring that user data is secure. 
+\n\n");
+
 
     let test_password = "123123123";
 
     let hash_password = hash(test_password, DEFAULT_COST).expect("Failed to hash password");
     let _ = verify(test_password, &hash_password);
     println!("Password: {}", test_password);
-    println!("Hashed password: {}", hash_password);
+    println!("Hashed password: {}\n", hash_password);
+
+    get_user_input();
+}
+
+fn get_user_input(){
+
+    println!("Please input a password: ");
+    
+    //create a buffer
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("(-) Failed at reading stdin");
+    //println!("{}", buffer.trim());
+    
+
+    //let buffer_ref: &str = &buffer.trim();
+
+    let hashed_password = hash(buffer.clone(), DEFAULT_COST).expect("(-) Failed password hashing");
+    let true_hash = verify(buffer.clone(), &hashed_password).expect("(-) Failed at asserting hash and verify");
+    assert_eq!(true_hash, true);
+
+    println!("Password: {} \nHashed: {}\n", buffer, hashed_password);
+
 }
 
 #[test]
